3a25f3c8b2cb17a3a4f14e54541eedbcc162ff0b
Thomas Steiner <tomac@google.com>
2023-05-08 10:20:20 +0200

    Fix prettier fixes

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,17 @@
 ((browser) => {
   browser.runtime.onConnect.addListener((devToolsConnection) => {
     // Assign the listener function to a variable so we can remove it later.
-    const devToolsListener = ({ tabId, name }, port) =>
+    const devToolsListener = ({ tabId, name }, port) => {
       name === 'init' && port.postMessage(`Connected: ${tabId}`);
+    }
 
     devToolsConnection.onMessage.addListener(devToolsListener);
-    devToolsConnection.onDisconnect.addListener(() =>
-      devToolsConnection.onMessage.removeListener(devToolsListener),
-    );
+    devToolsConnection.onDisconnect.addListener(() => {
+      devToolsConnection.onMessage.removeListener(devToolsListener);
+    });
 
-    browser.tabs.onUpdated.addListener(() =>
-      devToolsConnection.postMessage({ name: 'navigation' }),
-    );
+    browser.tabs.onUpdated.addListener(() => {
+      devToolsConnection.postMessage({ name: 'navigation' });
+    });
   });
 })(chrome || browser);
