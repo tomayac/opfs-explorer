41ab2daaac53cf070c25fda6ea48931eeddf5bf4
Thomas Steiner <tomac@google.com>
2023-05-08 09:52:18 +0200

    Re-add the IIFE

--- serviceworker.js
+++ serviceworker.js
@@ -1,12 +1,16 @@
-(chrome || browser).runtime.onConnect.addListener((devToolsConnection) => {
-  // Assign the listener function to a variable so we can remove it later.
-  const devToolsListener = ({ tabId, name },port) => 
-    name === 'init' && port.postMessage(`Connected: ${tabId}`);
+((browser) => {
+  browser.runtime.onConnect.addListener((devToolsConnection) => {
+    // Assign the listener function to a variable so we can remove it later.
+    const devToolsListener = ({ tabId, name }, port) =>
+      name === 'init' && port.postMessage(`Connected: ${tabId}`);
 
-  devToolsConnection.onMessage.addListener(devToolsListener);
-  devToolsConnection.onDisconnect.addListener(() => 
-    devToolsConnection.onMessage.removeListener(devToolsListener));
+    devToolsConnection.onMessage.addListener(devToolsListener);
+    devToolsConnection.onDisconnect.addListener(() =>
+      devToolsConnection.onMessage.removeListener(devToolsListener),
+    );
 
-  (chrome || browser).tabs.onUpdated.addListener(() => 
-    devToolsConnection.postMessage({ name: 'navigation' }));
-});
+    browser.tabs.onUpdated.addListener(() =>
+      devToolsConnection.postMessage({ name: 'navigation' }),
+    );
+  });
+})(chrome || browser);
